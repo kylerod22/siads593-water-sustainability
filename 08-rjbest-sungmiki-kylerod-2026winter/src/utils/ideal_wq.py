eca8e14efe62956b39bcf4be0a0a5ed5544ed18d
student <rjbest@umich.edu>
2026-02-13 09:36:43 +0000

    Change binary values to float

--- 08-rjbest-sungmiki-kylerod-2026winter/src/utils/ideal_wq.py
+++ 08-rjbest-sungmiki-kylerod-2026winter/src/utils/ideal_wq.py
@@ -22,17 +22,17 @@ def ideal_wq(df, path='data/wq.csv'):
     df.describe().round().astype(int)
 
     # Add a column for each key value, and label it 1 if the instances value is within the ideal range, and 0 otherwise.
-    df['DRP'] = [1 if _ < 100 else 0 for _ in df['dissolved reactive phosphorus']]
-    df['EC'] = [1 if _ < 800 else 0 for _ in df['electrical conductance']]
-    df['Alkalinity'] = [1 if _ > 20 and _ < 200 else 0 for _ in df['total alkalinity']]
+    df['DRP'] = [1.0 if _ < 100 else 0.0 for _ in df['dissolved reactive phosphorus']]
+    df['EC'] = [1.0 if _ < 800 else 0.0 for _ in df['electrical conductance']]
+    df['Alkalinity'] = [1.0 if _ > 20 and _ < 200 else 0.0 for _ in df['total alkalinity']]
 
     # Here are DataFrames with only ideal or undeal water quality
-    ideal = df[(df['DRP']==1) & (df['EC']==1) & (df['Alkalinity']==1)]
-    unideal = df[(df['DRP']==0) & (df['EC']==0) & (df['Alkalinity']==0)]
+    # ideal = df[(df['DRP']==1) & (df['EC']==1) & (df['Alkalinity']==1)]
+    # unideal = df[(df['DRP']==0) & (df['EC']==0) & (df['Alkalinity']==0)]
 
     # Add ideal/unideal columns
     df = df.assign(how_good=df['Alkalinity']+df['DRP']+df['EC'])
-    df['ideal'] = np.where(df['how_good'] > 2, 1, 0)
-    df['unideal'] = np.where(df['how_good'] < 1, 1, 0)
+    df['ideal'] = np.where(df['how_good'] > 2, 1.0, 0.0)
+    df['unideal'] = np.where(df['how_good'] < 1, 1.0, 0.0)
 
     return df
